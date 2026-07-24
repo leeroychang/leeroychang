1af42862895b9e4702364cdbfdaed80eb4f3db41
Everoy <leeroychang123@gmail.com>
2026-07-24 18:06:14 +0800

    Updated Portfolio

--- theme_script.js
+++ theme_script.js
@@ -1,10 +1,22 @@
 const modeToggle = document.getElementById('mode-toggle');
+const modeIcon = document.getElementById('mode-icon');
 
 modeToggle.addEventListener('click', () => {
     document.body.style.transition = 'background-color 0.3s, color 0.3s';
     const isLightMode = document.body.classList.toggle('light-mode');
     localStorage.setItem('themeMode', isLightMode ? 'light' : 'dark');
-    modeToggle.textContent = isLightMode ? 'Toggle Dark Mode' : 'Toggle Light Mode';
+    modeIcon.classList.toggle('fa-sun-o');
+    modeIcon.classList.toggle('fa-moon-o');
+})
+
+modeToggle.addEventListener('mouseenter', () => {
+    modeIcon.classList.toggle('fa-sun-o');
+    modeIcon.classList.toggle('fa-moon-o');
+})
+
+modeToggle.addEventListener('mouseleave', () => {
+    modeIcon.classList.toggle('fa-sun-o');
+    modeIcon.classList.toggle('fa-moon-o');
 })
 
 
@@ -14,5 +26,6 @@ const systemMode = window.matchMedia('(prefers-color-scheme: light)').matches;
 if (themeMode === 'light' || (themeMode!=='dark' && systemMode))
 {
     document.body.classList.add('light-mode');
-    modeToggle.textContent = "Toggle Dark Mode";
+    modeIcon.classList.toggle('fa-sun-o');
+    modeIcon.classList.toggle('fa-moon-o');
 }
